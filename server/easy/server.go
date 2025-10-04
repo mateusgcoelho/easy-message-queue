e9ab9227b43e01a70df9d5f179cb157f9ea4dc9c
João Mateus <mateusgamescoelho547@gmail.com>
2025-10-03 22:58:38 -0300

    feat: add a startup of a tcp's server

--- server/easy/server.go
+++ server/easy/server.go
@@ -1,6 +1,9 @@
 package easy
 
-import "log"
+import (
+	"log"
+	"net"
+)
 
 type easyServer struct {
 }
@@ -10,6 +13,21 @@ func NewServer() *easyServer {
 }
 
 func (s *easyServer) Start(addr string) error {
-	log.Println("Server started at", addr)
-	return nil
+	log.Printf("starting server on: %s", addr)
+
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("failed to accept connection: %v", err)
+			continue
+		}
+
+		log.Printf("accepted connection from: %s", conn.RemoteAddr().String())
+	}
 }
